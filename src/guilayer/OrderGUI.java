f55920c2af25dbf4c8b0a0e44b9f1decbe9c117f
jacobnehme <39905774+jacobnehme@users.noreply.github.com>
2018-06-05 09:43:35 +0200

    JN
    +OrderGUI

--- src/guilayer/OrderGUI.java
+++ src/guilayer/OrderGUI.java
@@ -6,6 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JTabbedPane;
+import javax.swing.JButton;
+import java.awt.TextArea;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
 
 public class OrderGUI extends JFrame {
 
@@ -31,12 +40,66 @@ public class OrderGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public OrderGUI() {
+		setTitle("Salg");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("Filer");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmQuit = new JMenuItem("Afslut");
+		mnFile.add(mntmQuit);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("New tab", null, panel, null);
+		panel.setLayout(new BorderLayout(15, 0));
+		
+		JPanel panel_4 = new JPanel();
+		panel.add(panel_4, BorderLayout.SOUTH);
+		panel_4.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnNewButton = new JButton("Tilbage");
+		panel_4.add(btnNewButton, BorderLayout.WEST);
+		
+		JPanel panel_5 = new JPanel();
+		panel_4.add(panel_5, BorderLayout.EAST);
+		
+		JButton btnNewButton_2 = new JButton("Bek\u00E6ft");
+		panel_5.add(btnNewButton_2);
+		
+		JButton btnNewButton_1 = new JButton("Anuller");
+		panel_5.add(btnNewButton_1);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new TitledBorder(null, "Medarbejder", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.add(panel_6, BorderLayout.WEST);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBorder(new TitledBorder(null, "Kunde", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.add(panel_7, BorderLayout.CENTER);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Produkter", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.add(panel_8, BorderLayout.EAST);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
+		
+		JPanel panel_3 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_3, null);
 	}
 
 }
