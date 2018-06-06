74b64449fa86a5ed0df20929c9cc20ca88c7dcd0
Torres137 <Alexander_holm@hotmail.com>
2018-06-06 13:24:43 +0200

    Rediger knapper

--- src/guilayer/ProductGUI.java
+++ src/guilayer/ProductGUI.java
@@ -89,7 +89,7 @@ public class ProductGUI extends JFrame {
 	 */
 	public ProductGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700, 463);
+		setBounds(100, 100, 700, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
