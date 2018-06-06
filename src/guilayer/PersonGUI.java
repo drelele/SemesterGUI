779e30b373382b90b29df6eff9a824c0654de69e
stefa <stefa@DESKTOP-MES112K>
2018-06-06 15:02:48 +0200

    rediger person - Stefan

--- src/guilayer/PersonGUI.java
+++ src/guilayer/PersonGUI.java
@@ -28,9 +28,28 @@ import javax.swing.UIManager;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.FlowLayout;
+
+import controllerlayer.PersonController;
+import modellayer.*;
+import javax.swing.border.LineBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JTextPane;
+import java.awt.event.InputMethodListener;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class PersonGUI extends JFrame {
 
+	
+	private PersonController personCtrl;
+	
+	private Person returnPerson;
+	
 	private JPanel contentPane;
 	private JTextField textFieldCity;
 	private JTextField textFieldPostalCode;
@@ -42,7 +61,17 @@ public class PersonGUI extends JFrame {
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_7;
-	private JTextField textField_5;
+	private JTextField textFieldfindPerson;
+	private JTextField textFieldReturnAddress;
+	private JTextField textFieldReturnCity;
+	private JTextField textFieldReturnPostalCode;
+	private JTextField textFieldReturnName;
+	private JTextField textFieldConfirmName;
+	private JTextField textFieldConfirmPhone;
+	private JTextField textFieldConfirmAddress;
+	private JTextField textFieldConfirmCity;
+	private JTextField textFieldConfirmPostalCode;
+	private JTextField textField_16;
 
 	/**
 	 * Launch the application.
@@ -66,7 +95,7 @@ public class PersonGUI extends JFrame {
 	public PersonGUI() {
 		setTitle("Person");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-setBounds(100, 100, 700, 400);
+setBounds(100, 100, 700, 471);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -82,41 +111,15 @@ setBounds(100, 100, 700, 400);
 		contentPane.setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(5, 5, 674, 330);
+		tabbedPane.setBounds(5, 5, 674, 395);
 		contentPane.add(tabbedPane);
 		
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Opret person", null, panel, null);
 		panel.setLayout(null);
 		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBounds(0, 262, 669, 40);
-		panel.add(panel_4);
-		panel_4.setLayout(new BorderLayout(0, 0));
-		
-		JButton btnNewButton = new JButton("Tilbage");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel_4.add(btnNewButton, BorderLayout.WEST);
-		
-		JPanel panel_5 = new JPanel();
-		panel_4.add(panel_5, BorderLayout.EAST);
-		
-		JButton btnNewButton_2 = new JButton("Bek\u00E6ft");
-		btnNewButton_2.setEnabled(false);
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		panel_5.add(btnNewButton_2);
-		
-		JButton btnNewButton_1 = new JButton("Anuller");
-		panel_5.add(btnNewButton_1);
-		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 11, 324, 253);
+		panel_1.setBounds(0, 11, 324, 304);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
@@ -179,7 +182,7 @@ setBounds(100, 100, 700, 400);
 		panel_1.add(textFieldName);
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(334, 11, 335, 253);
+		panel_2.setBounds(334, 11, 335, 304);
 		panel.add(panel_2);
 		panel_2.setLayout(null);
 		
@@ -296,51 +299,382 @@ setBounds(100, 100, 700, 400);
 			confirmPerson.setEnabled(true);
 		}
 		
-		confirmPerson.setBounds(233, 219, 81, 23);
+		confirmPerson.setBounds(233, 270, 81, 23);
 		panel_1.add(confirmPerson);
+		
+		JButton button_5 = new JButton("Annuller");
+		button_5.setBounds(471, 333, 89, 23);
+		panel.add(button_5);
+		
+		JButton button_7 = new JButton("Tilbage");
+		button_7.setBounds(0, 333, 89, 23);
+		panel.add(button_7);
+		
+		JButton btnNewButton_2 = new JButton("Bek\u00E6ft");
+		btnNewButton_2.setBounds(570, 333, 89, 23);
+		panel.add(btnNewButton_2);
+		btnNewButton_2.setEnabled(false);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBorder(null);
+		tabbedPane.addTab("\u00C6ndre person", null, panel_7, null);
+		panel_7.setLayout(null);
+		
+		
+		
 		
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(Color.DARK_GRAY);
-		tabbedPane.addTab("Rediger person", null, panel_3, null);
-		tabbedPane.setEnabledAt(1, false);
-		panel_3.setLayout(null);
+		panel_3.setBounds(0, 0, 669, 71);
+		panel_7.add(panel_3);
 		
-		JPanel panel_6 = new JPanel();
-		panel_6.setBounds(174, 11, 331, 49);
-		panel_6.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Find Person", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
-		panel_6.setBackground(Color.DARK_GRAY);
-		panel_3.add(panel_6);
-		GridBagLayout gbl_panel_6 = new GridBagLayout();
-		gbl_panel_6.columnWidths = new int[]{0, 0, 0, 0};
-		gbl_panel_6.rowHeights = new int[]{0, 0};
-		gbl_panel_6.columnWeights = new double[]{0.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_6.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel_6.setLayout(gbl_panel_6);
+		JPanel panel_10 = new JPanel();
+		panel_10.setBackground(Color.DARK_GRAY);
+		panel_3.add(panel_10);
+		panel_10.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel label_5 = new JLabel("Indtast telefonnr.");
-		label_5.setForeground(Color.WHITE);
-		GridBagConstraints gbc_label_5 = new GridBagConstraints();
-		gbc_label_5.insets = new Insets(0, 0, 0, 5);
-		gbc_label_5.gridx = 0;
-		gbc_label_5.gridy = 0;
-		panel_6.add(label_5, gbc_label_5);
+		JPanel panel_11 = new JPanel();
+		panel_11.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Find Person", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
+		panel_11.setBackground(Color.DARK_GRAY);
+		panel_10.add(panel_11);
+		GridBagLayout gbl_panel_11 = new GridBagLayout();
+		gbl_panel_11.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_panel_11.rowHeights = new int[]{0, 0};
+		gbl_panel_11.columnWeights = new double[]{0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_11.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_11.setLayout(gbl_panel_11);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
-		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_5.insets = new Insets(0, 0, 0, 5);
-		gbc_textField_5.gridx = 1;
-		gbc_textField_5.gridy = 0;
-		panel_6.add(textField_5, gbc_textField_5);
+		JLabel label_16 = new JLabel("Indtast telefonnr.");
+		label_16.setForeground(Color.WHITE);
+		GridBagConstraints gbc_label_16 = new GridBagConstraints();
+		gbc_label_16.insets = new Insets(0, 0, 0, 5);
+		gbc_label_16.gridx = 0;
+		gbc_label_16.gridy = 0;
+		panel_11.add(label_16, gbc_label_16);
 		
-		JButton button = new JButton("Find");
-		GridBagConstraints gbc_button = new GridBagConstraints();
-		gbc_button.gridx = 2;
-		gbc_button.gridy = 0;
-		panel_6.add(button, gbc_button);
+		textFieldfindPerson = new JTextField();
+		textFieldfindPerson.setColumns(10);
+		GridBagConstraints gbc_textFieldfindPerson = new GridBagConstraints();
+		gbc_textFieldfindPerson.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldfindPerson.insets = new Insets(0, 0, 0, 5);
+		gbc_textFieldfindPerson.gridx = 1;
+		gbc_textFieldfindPerson.gridy = 0;
+		panel_11.add(textFieldfindPerson, gbc_textFieldfindPerson);
 		
+
 		
+		JButton button_1 = new JButton("Annuller");
+		button_1.setBounds(471, 333, 89, 23);
+		panel_7.add(button_1);
+		
+		JButton button_ConfirmAll = new JButton("Bekr\u00E6ft");
+		button_ConfirmAll.setEnabled(false);
+		button_ConfirmAll.addActionListener(new ActionListener() {
+			
+			PersonController personCtrl = new PersonController();
+			public void actionPerformed(ActionEvent e) { 
+				
+				String newName = textFieldConfirmName.getText();
+				String newAddress = textFieldConfirmAddress.getText();
+				String newCity = textFieldConfirmCity.getText();
+				String newPostalCode = textFieldConfirmPostalCode.getText();
+				
+				personCtrl.updatePerson(returnPerson, newName, returnPerson.getPhone(), newAddress, newCity, newPostalCode);
+				
+				JPanel personUpdated = new JPanel();
+				personUpdated.setBounds(237, 140, 205, 115);
+				panel_7.add(personUpdated);
+				personUpdated.setBackground(Color.DARK_GRAY);
+				personUpdated.setForeground(Color.DARK_GRAY);
+				personUpdated.setLayout(null);
+				
+				
+				JLabel lblPersonenBlevRedigeret = new JLabel("Personen blev opdateret");
+				lblPersonenBlevRedigeret.setForeground(Color.WHITE);
+				lblPersonenBlevRedigeret.setBackground(Color.WHITE);
+				lblPersonenBlevRedigeret.setBounds(38, 29, 137, 29);
+				personUpdated.add(lblPersonenBlevRedigeret);
+				
+				
+				
+				JButton btnLuk = new JButton("Luk ");
+				
+				btnLuk.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent arg0) {
+						textFieldReturnName.setText("");
+						textFieldReturnAddress.setText("");
+						textFieldReturnCity.setText("");
+						textFieldReturnPostalCode.setText("");
+						
+						textFieldConfirmName.setText("");
+						textFieldConfirmPhone.setText("");
+						textFieldConfirmAddress.setText("");
+						textFieldConfirmCity.setText("");
+						textFieldConfirmPostalCode.setText("");
+						personUpdated.setVisible(false);
+						button_ConfirmAll.setEnabled(false);
+						
+						
+						
+					}
+				});
+				
+				btnLuk.setBounds(59, 69, 89, 23);
+				personUpdated.add(btnLuk);
+				
+				
+			}
+		});
+		
+
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBorder(null);
+		panel_8.setLayout(null);
+		panel_8.setBounds(0, 76, 324, 253);
+		panel_7.add(panel_8);
+		
+		JLabel label_5 = new JLabel("Navn");
+		label_5.setBounds(10, 3, 48, 14);
+		panel_8.add(label_5);
+		
+		JLabel label_7 = new JLabel("Adresse");
+		label_7.setBounds(10, 28, 48, 14);
+		panel_8.add(label_7);
+		
+		JLabel label_8 = new JLabel("By");
+		label_8.setBounds(10, 53, 48, 14);
+		panel_8.add(label_8);
+		
+		JLabel label_9 = new JLabel("Post nr.");
+		label_9.setBounds(10, 78, 48, 14);
+		panel_8.add(label_9);
+		
+	
+		
+		JButton button_ConfirmChanges = new JButton("Bek\u00E6ft");
+		button_ConfirmChanges.setEnabled(false);
+		button_ConfirmChanges.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(button_ConfirmChanges.isEnabled()) {
+				button_ConfirmAll.setEnabled(true);
+				textFieldConfirmName.setText(textFieldReturnName.getText());
+				textFieldConfirmAddress.setText(textFieldReturnAddress.getText());
+				textFieldConfirmCity.setText(textFieldReturnCity.getText());
+				textFieldConfirmPostalCode.setText(textFieldReturnPostalCode.getText());
+				textFieldConfirmPhone.setText(returnPerson.getPhone());
+				} else {
+					button_ConfirmAll.setEnabled(false);
+				}
+			}
+		});
+
+		button_ConfirmChanges.setBounds(225, 219, 89, 23);
+		panel_8.add(button_ConfirmChanges);
+		button_ConfirmAll.setBounds(570, 333, 89, 23);
+		panel_7.add(button_ConfirmAll);
+		
+		JButton button_4 = new JButton("Tilbage");
+		button_4.setBounds(0, 333, 89, 23);
+		panel_7.add(button_4);
+		
+		JPanel panel_9 = new JPanel();
+		panel_9.setLayout(null);
+		panel_9.setBounds(334, 76, 335, 253);
+		panel_7.add(panel_9);
+		
+		textFieldReturnName = new JTextField();
+		textFieldReturnName.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				button_ConfirmChanges.setEnabled(true);
+			}
+		});
+		textFieldReturnName.setColumns(10);
+		textFieldReturnName.setBounds(93, 0, 221, 20);
+		panel_8.add(textFieldReturnName);
+		
+		textFieldReturnAddress = new JTextField();
+		textFieldReturnAddress.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				button_ConfirmChanges.setEnabled(true);
+			}
+		});
+		textFieldReturnAddress.setColumns(10);
+		textFieldReturnAddress.setBounds(93, 25, 221, 20);
+		panel_8.add(textFieldReturnAddress);
+		
+		textFieldReturnCity = new JTextField();
+		textFieldReturnCity.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				button_ConfirmChanges.setEnabled(true);
+			}
+		});
+		textFieldReturnCity.setColumns(10);
+		textFieldReturnCity.setBounds(93, 50, 221, 20);
+		panel_8.add(textFieldReturnCity);
+		
+		textFieldReturnPostalCode = new JTextField();
+		textFieldReturnPostalCode.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				button_ConfirmChanges.setEnabled(true);
+			}
+		});
+		textFieldReturnPostalCode.setColumns(10);
+		textFieldReturnPostalCode.setBounds(93, 75, 221, 20);
+		panel_8.add(textFieldReturnPostalCode);
+		
+		textFieldConfirmName = new JTextField();
+		textFieldConfirmName.setEditable(false);
+		textFieldConfirmName.setColumns(10);
+		textFieldConfirmName.setBounds(109, 0, 216, 20);
+		panel_9.add(textFieldConfirmName);
+		
+		textFieldConfirmPhone = new JTextField();
+		textFieldConfirmPhone.setEditable(false);
+		textFieldConfirmPhone.setColumns(10);
+		textFieldConfirmPhone.setBounds(109, 25, 216, 20);
+		panel_9.add(textFieldConfirmPhone);
+		
+		textFieldConfirmAddress = new JTextField();
+		textFieldConfirmAddress.setEditable(false);
+		textFieldConfirmAddress.setColumns(10);
+		textFieldConfirmAddress.setBounds(109, 50, 216, 20);
+		panel_9.add(textFieldConfirmAddress);
+		
+		textFieldConfirmCity = new JTextField();
+		textFieldConfirmCity.setEditable(false);
+		textFieldConfirmCity.setColumns(10);
+		textFieldConfirmCity.setBounds(109, 75, 216, 20);
+		panel_9.add(textFieldConfirmCity);
+		
+		textFieldConfirmPostalCode = new JTextField();
+		textFieldConfirmPostalCode.setEditable(false);
+		textFieldConfirmPostalCode.setColumns(10);
+		textFieldConfirmPostalCode.setBounds(109, 100, 216, 20);
+		panel_9.add(textFieldConfirmPostalCode);
+		
+		JLabel label_10 = new JLabel("Navn");
+		label_10.setBounds(10, 3, 48, 14);
+		panel_9.add(label_10);
+		
+		JLabel label_11 = new JLabel("Telefon");
+		label_11.setBounds(10, 28, 48, 14);
+		panel_9.add(label_11);
+		
+		JLabel label_12 = new JLabel("Adresse");
+		label_12.setBounds(10, 53, 48, 14);
+		panel_9.add(label_12);
+		
+		JLabel label_13 = new JLabel("By");
+		label_13.setBounds(10, 78, 48, 14);
+		panel_9.add(label_13);
+		
+		JLabel label_14 = new JLabel("Post nr.");
+		label_14.setBounds(10, 103, 48, 14);
+		panel_9.add(label_14);
+		
+		textField_16 = new JTextField();
+		textField_16.setEditable(false);
+		textField_16.setColumns(10);
+		textField_16.setBounds(109, 148, 216, 20);
+		panel_9.add(textField_16);
+		
+		JCheckBox checkBox = new JCheckBox("Medarbejdernummer");
+		checkBox.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		checkBox.setEnabled(false);
+		checkBox.setBounds(214, 127, 111, 14);
+		panel_9.add(checkBox);
+		
+		JCheckBox checkBox_1 = new JCheckBox("Kundenummer");
+		checkBox_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		checkBox_1.setEnabled(false);
+		checkBox_1.setBounds(109, 127, 91, 14);
+		panel_9.add(checkBox_1);
+		
+		JButton btnDeletePerson = new JButton("Slet person");
+		btnDeletePerson.setForeground(Color.WHITE);
+		btnDeletePerson.setBackground(new Color(204, 0, 0));
+		btnDeletePerson.setBounds(10, 219, 101, 23);
+		panel_9.add(btnDeletePerson);
+		
+		JButton button_3 = new JButton("Find");
+		button_3.addActionListener(new ActionListener() {
+			
+			PersonController personCtrl = new PersonController();
+			public void actionPerformed(ActionEvent e) {				
+				try {	
+				returnPerson = getPerson(textFieldfindPerson.getText());
+				textFieldReturnName.setText(returnPerson.getName());
+				textFieldReturnAddress.setText(returnPerson.getAddress());
+				textFieldReturnCity.setText(returnPerson.getCity());
+				textFieldReturnPostalCode.setText(returnPerson.getPostalCode());
+				
+				textFieldConfirmName.setText("");
+				textFieldConfirmPhone.setText("");
+				textFieldConfirmAddress.setText("");
+				textFieldConfirmCity.setText("");
+				textFieldConfirmPostalCode.setText("");
+				
+				button_ConfirmChanges.setEnabled(false);
+				button_ConfirmAll.setEnabled(false);;
+				} catch (NullPointerException ee) {
+					errorString();
+				}
+			}
+			
+			private Person getPerson(String phone) {
+				return personCtrl.getPerson(phone);
+				
+			}
+			
+		
+			private void errorString() {
+				
+				JPanel errorPanel = new JPanel();
+				errorPanel.setBounds(237, 140, 205, 115);
+				panel_7.add(errorPanel);
+				errorPanel.setBackground(Color.DARK_GRAY);
+				errorPanel.setForeground(Color.DARK_GRAY);
+				errorPanel.setLayout(null);
+				
+				JButton buttonAccept = new JButton("Accepter");
+				buttonAccept.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						errorPanel.setVisible(false);
+					}
+				});
+
+				buttonAccept.setBounds(56, 81, 89, 23);
+				errorPanel.add(buttonAccept);
+				
+				JTextPane txtpnDuSkalSkrive = new JTextPane();
+				txtpnDuSkalSkrive.setEditable(false);
+				txtpnDuSkalSkrive.setForeground(Color.WHITE);
+				txtpnDuSkalSkrive.setBackground(Color.DARK_GRAY);
+				txtpnDuSkalSkrive.setText("Personen findes ikke");
+				txtpnDuSkalSkrive.setBounds(45, 11, 125, 47);
+				errorPanel.add(txtpnDuSkalSkrive);
+				
+				
+			}
+		});
+		
+		GridBagConstraints gbc_button_3 = new GridBagConstraints();
+		gbc_button_3.gridx = 2;
+		gbc_button_3.gridy = 0;
+		panel_11.add(button_3, gbc_button_3);
 		
 		
 	}
